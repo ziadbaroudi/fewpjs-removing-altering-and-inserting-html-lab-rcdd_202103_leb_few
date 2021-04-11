fccc537e3f9ea7287545baad45b7e75f8ba312b6
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-11 19:32:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 document.querySelector("main#main").remove();
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
-newHeader.innerText = "ziadbaroudi is the champion! ";
+newHeader.innerHTML = "ziadbaroudi is the champion! ";
 newHeader.className = "victory";
 document.body.appendChild(newHeader);
