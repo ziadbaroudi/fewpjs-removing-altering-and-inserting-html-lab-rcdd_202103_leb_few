1d45450e6f494ef64eaf9f088f3c9cf167bdc4e2
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-11 19:19:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ document.querySelector("main#main").remove();
 /* Create your new element here and assign it to newHeader */
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerText = "Steven is the champion!";
+newHeader.innerText = "nvjfd is the champion";
 newHeader.className = "victory";
