0d362a26d890fb9a38c6822645acdec1fd11c173
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-11 19:32:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "ziadbaroudi is the champion! ";
+newHeader.className = "victory";
+document.body.appendChild(newHeader);
